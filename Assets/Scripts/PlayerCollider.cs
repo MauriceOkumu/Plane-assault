b570c256b7b2d45bdcdb2adc0c4fb704ee3db292
maurice <maurice.okumu@thoughtworks.com>
2021-03-09 10:41:40 -0600

    Add player explosion sound |Maurice

--- Assets/Scripts/PlayerCollider.cs
+++ Assets/Scripts/PlayerCollider.cs
@@ -6,9 +6,14 @@ using UnityEngine.SceneManagement;
 public class PlayerCollider : MonoBehaviour
 {
     [SerializeField] ParticleSystem explosion;
-
+    [SerializeField] AudioClip efx;
+    AudioSource asource;
+    private void Start() {
+        asource = GetComponent<AudioSource>();
+    }
     void OnCollisionEnter(Collision other) {
         explosion.Play();
+        if(!asource.isPlaying) asource.PlayOneShot(efx);
         GetComponent<PlayerController>().enabled = false;
         Invoke("ReloadLevel", 1f);
     }
